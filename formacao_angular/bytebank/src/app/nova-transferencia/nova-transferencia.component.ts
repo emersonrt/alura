e948b90a59d3e12e0e86f66cee987b3e29376d8e
Emerson Teixeira <emersonrosateixeira@hotmail.com>
2021-12-17 12:19:28 -0300

    finalização curso angular

--- formacao_angular/bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
+++ formacao_angular/bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,40 +1,44 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
 import { Transferencia } from '../models/transferencia.model';
 import { TransferenciaService } from '../services/transferencia.service';
 
 @Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
-  styleUrls: ['./nova-transferencia.component.scss']
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent implements OnInit {
-
   @Output() aoTransferir = new EventEmitter<any>();
   @Output() valoresComErro = new EventEmitter<String>();
 
   valor: number;
   destino: string;
 
-  constructor(private service: TransferenciaService) { }
+  constructor(private service: TransferenciaService, private router: Router) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   transferir() {
     console.log('Solicitada nova transferência');
-    const valorEmitir: Transferencia = { valor: this.valor, destino: this.destino };
-    this.service.adicionar(valorEmitir).subscribe(resultado => {
-      console.log(resultado);
-      this.limparCampos();
-    },
-    error => {
-      console.error(error);
-    });
+
+    const valorEmitir: Transferencia = {
+      valor: this.valor,
+      destino: this.destino,
+    };
+
+    this.service.adicionar(valorEmitir).subscribe(
+      (resultado) => {
+        console.log(resultado);
+        this.router.navigateByUrl('extrato');
+      },
+      (error) => console.error(error)
+    );
+
   }
 
   limparCampos() {
     this.valor = 0;
     this.destino = '0';
   }
-
 }
